dfa5eed57c21fb898c902a3044a8e905c4121f34
maryam rezaei <mari.re9978@gmail.com>
2024-04-20 16:35:53 +0330

    delete extra item

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import favoriteSlice from '../features/favorites/favoriteSlice.js';
-import {characterSlice} from "../features/ characters/characterSlice.js";
-export  const store = configureStore({
-    reducer:{
-       favorite:favoriteSlice,
-        character : characterSlice
-    }
-})
